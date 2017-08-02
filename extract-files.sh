9f08b966146cd05fed95ad43338c36fee2e9e138
Michael Bestas <mkbestas@lineageos.org>
2017-08-02 19:15:00 +0300

    seed: Configure TWRP
    Change-Id: Iaa3cab86ad51d41b9190694adeef6d63b02ba81e

--- extract-files.sh
+++ extract-files.sh
@@ -56,8 +56,13 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$KINEAGE_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
+
+TWRP_QSEECOMD="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/recovery/root/sbin/qseecomd
+
+sed -i "s|/system/bin/linker|/sbin/linker\x0\x0\x0\x0\x0\x0|g" "$TWRP_QSEECOMD"
 
 "$MY_DIR"/setup-makefiles.sh
