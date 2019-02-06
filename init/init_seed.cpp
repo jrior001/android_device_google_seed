01dd22a1029f10c7be7f0535188d42a0c818e957
jrior001 <jriordan001@gmail.com>
2019-02-05 22:33:58 -0500

    init: fix compile for O
    Change-Id: I5fb636431fab29b7dc3bd11358c15c8de27c1925

--- init/init_seed.cpp
+++ init/init_seed.cpp
@@ -32,12 +32,17 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
+#include <android-base/properties.h>
+
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
 
 #include "init_msm8916.h"
+
+using android::base::GetProperty;
+using android::init::property_set;
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
@@ -87,7 +92,7 @@ void init_target_properties()
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", "8m");
 
-    std::string name = property_get("ro.product.name");
+    std::string name = GetProperty("ro.product.name","");
 
     if (name == "ctih220") {
         property_override("ro.product.device", "ctih220_sprout");
